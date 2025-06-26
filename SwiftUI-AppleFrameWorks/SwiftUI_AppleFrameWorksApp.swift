677675f2947d09aaaf1839ff788872cefd19dd05
Gabriel <moraesnicol@gmail.com>
2025-06-26 13:30:36 -0300

    init

---
+++ SwiftUI-AppleFrameWorks/SwiftUI_AppleFrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameWorksApp.swift
+//  SwiftUI-AppleFrameWorks
+//
+//  Created by Gabriel Moraes on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
